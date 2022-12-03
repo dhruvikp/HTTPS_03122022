cc3acffdf5c04745fde112a4603f52eb0d9b53a5
Dhruvik <dhruviksparikh@gmail.com>
2022-12-03 23:21:22 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/Https03122022Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Https03122022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Https03122022Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/simplilearn/controller/GreetController.java
@@ -0,0 +1,13 @@
+package com.simplilearn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "hello simplilearn!! Welcome to SSL example!!";
+	}
+}
